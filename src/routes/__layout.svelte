f418b2196bf917fbcba394ec8de4a184bb65897f
Jen Garcia <jen@haverstack.com>
2021-08-15 01:31:42 -0400

    Refine the main layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,13 +5,15 @@
 
 <MetaTitle title="cuibonobo" />
 
-<header class="items-center container justify-between flex bg-white p-4 my-4 mx-auto">
-  <div class="text-2xl font-bold inline-flex items-center">cuibonobo</div>
-  <nav class="flex">
-    <a class="ml-5 px-2 py-1" href="/">Home</a>
-    <a class="ml-5 px-2 py-1" href="/ephemera/">Ephemera</a>
-    <a class="ml-5 px-2 py-1" href="/articles/">Articles</a>
-    <a class="ml-5 px-2 py-1" href="/about/">About</a>
+<header id="top-bar" class="container justify-between flex flex-col md:flex-row bg-white py-2 my-4 mx-auto">
+  <div class="text-2xl font-bold inline-flex pl-2">
+    <a class="px-2 py-1" href="/">cuibonobo</a>
+  </div>
+  <nav id="main-menu" class="flex flex-wrap pr-2">
+    <a class="mx-2 px-2 py-1" href="/">Home</a>
+    <a class="mx-2 px-2 py-1" href="/ephemera/">Ephemera</a>
+    <a class="mx-2 px-2 py-1" href="/articles/">Articles</a>
+    <a class="ml-2 px-2 py-1" href="/about/">About</a>
   </nav>
 </header>
 
@@ -19,12 +21,25 @@
   <slot />
 </div>
 
-<footer class="container mx-auto p-4 text-center text-sm text-gray-600">
-  Except where otherwise noted, everything by cuibonobo on this website is <a
-    class="underline"
-    title="Creative Commons CC0"
-    href="http://creativecommons.org/publicdomain/zero/1.0/"
-    target="_blank"
-    rel="noopener noreferrer">dedicated to the public domain</a
-  >.
+<footer class="container mx-auto p-4 md:px-0 grid grid-cols-1 md:grid-cols-2 text-sm text-gray-600">
+  <div class="mb-2">
+    Except where otherwise noted, everything by <a href="/">cuibonobo</a> on this website is <a
+      title="Creative Commons CC0"
+      href="http://creativecommons.org/publicdomain/zero/1.0/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >dedicated to the public domain</a>.
+  </div>
+  <div class="grid grid-rows-2 md:text-right">
+    <nav class="mb-2 md:mb-0">
+      <a class="md:ml-2" href="/ephemera/">Ephemera</a>
+      <a class="md:ml-2" href="/articles/">Articles</a>
+      <a class="md:ml-2" href="/about/">About</a>
+    </nav>
+    <div>
+      <a class="md:ml-2" href="https://twitter.com/cuibonobo" target="_blank" rel="noopener noreferrer">Twitter</a>
+      <a class="md:ml-2" href="https://github.com/cuibonobo" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a class="md:ml-2" href="https://instagram.com/cuibonobo" target="_blank" rel="noopener noreferrer">Instagram</a>
+    </div>
+  </div>
 </footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,26 @@
+const sansSerif = {
+  fontFamily: 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+};
+
 module.exports = {
   purge: ['./src/index.html', './src/**/*.svelte', './src/**/*.css'],
   darkMode: false,
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            fontFamily: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+            h1: sansSerif,
+            h2: sansSerif,
+            h3: sansSerif,
+            h4: sansSerif,
+            h5: sansSerif
+          }
+        }
+      }
+    }
+  },
   variants: {
     extend: {}
   },
