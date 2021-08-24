1174f4b797bc6bcdc6a9cae11b4a88db17d96a2d
Jen Garcia <jen@haverstack.com>
2021-08-24 00:06:53 -0400

    Add a new ephemera post; fix CLI formatting issues

--- src/cli.ts
+++ src/cli.ts
@@ -7,30 +7,28 @@ import { exec } from 'child_process';
 
 const dataDir = path.resolve('data');
 const writeFile = util.promisify(fs.writeFile);
-console.debug("dataDir", dataDir);
+console.debug('dataDir', dataDir);
 
 const program = new Command();
 program
   .command('new <postType> <slug>')
   .description('Create a new post with the given post type')
   .action(async (postType, slug) => {
-    console.debug("starting new");
+    console.debug('starting new');
     let postDir: string;
-    const postData = [
-      '---'
-    ];
-    console.debug("input data", postType, slug);
-    switch(postType) {
-      case "page":
+    const postData = ['---'];
+    console.debug('input data', postType, slug);
+    switch (postType) {
+      case 'page':
         postDir = path.join(dataDir, 'pages');
         postData.push('title: ');
         break;
-      case "article":
+      case 'article':
         postDir = path.join(dataDir, 'articles');
         postData.push('title: ');
         postData.push('tags: ');
         break;
-      case "ephemera":
+      case 'ephemera':
         postDir = path.join(dataDir, 'ephemera');
         slug = moment().format('YYYY/MM/DD-hh-mm-ss');
         break;
@@ -43,12 +41,11 @@ program
     postData.push('---');
     postData.push('');
     const postPath = path.join(postDir, `${slug}.md`);
-    console.debug("postPath", postPath);
-    console.debug("postData", postData);
+    console.debug('postPath', postPath);
+    console.debug('postData', postData);
     await writeFile(postPath, postData.join('\n'));
     exec(`code ${postPath}`);
-    console.debug("finished new");
+    console.debug('finished new');
   });
 
-  program.parse();
-  
+program.parse();
