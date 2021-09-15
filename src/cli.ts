80ea0eb67e8367093748322a7815dffcf5935ab1
Jen Garcia <jen@haverstack.com>
2021-09-14 23:37:50 -0400

    WIP for #2: Create post types for posts

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { Command } from 'commander';
 import moment from 'moment';
 import { exec } from 'child_process';
+import yaml from 'yaml';
 import { ulid } from 'ulid';
 import {
   getDataDir,
@@ -12,46 +13,86 @@ import {
   mkTempDir
 } from './lib/fs';
 
-enum PostTypes {
-  Page = 'pages',
-  Article = 'articles',
+enum PostTypeName {
+  Page = 'page',
+  Article = 'article',
   Ephemera = 'ephemera'
 }
+
+interface BasePost {
+  id: string;
+  published: Date;
+  updated: Date;
+  slug: string;
+}
+interface TitleMixin {
+  title: string;
+}
+interface PageType extends BasePost, TitleMixin {
+  type: PostTypeName.Page;
+}
+interface ArticleType extends BasePost, TitleMixin {
+  type: PostTypeName.Article;
+  tags: string;
+}
+interface EphemeraType extends BasePost {
+  type: PostTypeName.Ephemera;
+}
+
+type PostType = PageType | ArticleType | EphemeraType;
+
+const yamlDivider = '---';
+const yamlPlaceholder = '.';
+
+const getdefaultPostData = (postTypeName: PostTypeName): PostType => {
+  const now = new Date();
+  const postData = {
+    id: ulid(now.getTime()),
+    published: now,
+    updated: now,
+    slug: ''
+  };
+  switch (postTypeName) {
+    case PostTypeName.Page:
+      return { type: PostTypeName.Page, ...postData, title: yamlPlaceholder };
+    case PostTypeName.Article:
+      return {
+        type: PostTypeName.Article,
+        ...postData,
+        title: yamlPlaceholder,
+        tags: yamlPlaceholder
+      };
+    case PostTypeName.Ephemera:
+      return { type: PostTypeName.Ephemera, ...postData };
+  }
+};
 
 const program = new Command();
 program
   .command('new <postType> <slug>')
   .description('Create a new post with the given post type')
   .action(async (postType, slug) => {
+    if (Object.values(PostTypeName).indexOf(postType) < 0) {
+      console.error(`Can't create posts of type '${postType}'!`);
+      return;
+    }
     let postDir: string;
     const dataDir = getDataDir();
-    const now = moment();
-    const postData = ['---'];
-    switch (postType) {
-      case PostTypes.Page:
-        postDir = path.join(dataDir, 'pages');
-        postData.push('title: ');
-        break;
-      case PostTypes.Article:
-        postDir = path.join(dataDir, 'articles');
-        postData.push('title: ');
-        postData.push('tags: ');
-        break;
-      case PostTypes.Ephemera:
-        postDir = path.join(dataDir, 'ephemera', now.format('YYYY/MM'));
-        slug = now.format('DD-hh-mm-ss');
-        break;
-      default:
-        console.error(`Can't create posts of type '${postType}'!`);
-        return;
+    const postData = getdefaultPostData(postType);
+    const now = moment(postData.published);
+    if (postType === PostTypeName.Ephemera) {
+      postDir = path.join(dataDir, 'ephemera', now.format('YYYY/MM'));
+      postData.slug = now.format('DD-hh-mm-ss');
+    } else {
+      // FIXME: Converting the type name to plural for the directory name is hacky af
+      postDir = path.join(dataDir, `${postType}s`);
+      postData.slug = slug;
     }
-    postData.push(`published: ${moment().format()}`);
-    postData.push(`updated: ${moment().format()}`);
-    postData.push('---');
-    postData.push('');
+    const yamlLines = yaml.stringify(postData).split('\n');
+    const postOutput = [yamlDivider, ...yamlLines.slice(0, yamlLines.length - 1), yamlDivider, ''];
     await ensureDir(postDir);
     const postPath = path.join(postDir, `${slug}.md`);
-    await writeFile(postPath, postData.join('\n'));
+    await writeFile(postPath, postOutput.join('\n'));
     exec(`${getDefaultEditor()} "${postPath}"`);
   });
 
@@ -60,7 +101,7 @@ program
   .description('Migrates data to the latest version')
   .action(async () => {
     const tempDir = await mkTempDir();
-    for (const postType of Object.values(PostTypes)) {
+    for (const postType of Object.values(PostTypeName)) {
       const tempPostDir = path.join(tempDir, postType);
       const items = await getMarkdownItems([postType]);
       const output: JSON[] = [];
