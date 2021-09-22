6069a8bfd0b896b175023728f768222defe884c1
Jen Garcia <jen@haverstack.com>
2021-09-22 14:34:00 -0400

    Handle MarkDown parsing in a lib instead of a component

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
-  import marked from 'marked';
-  import xss from 'xss';
-  import xssDefaults from 'xss/lib/default.js';
-  import prism from 'prismjs';
-  import 'prismjs/components/prism-javascript.js';
-  import 'prismjs/components/prism-typescript.js';
-  import 'prismjs/components/prism-python.js';
-  import 'prismjs/components/prism-powershell.js';
-  import 'prismjs/components/prism-bash.js';
-  import 'prismjs/components/prism-batch.js';
-
-  marked.setOptions({
-    highlight: (code, lang) => {
-      if (prism.languages[lang]) {
-        return prism.highlight(code, prism.languages[lang], lang);
-      } else {
-        return code;
-      }
-    }
-  });
+  import { markdownToHtml } from '@lib/parser';
 
   export let markdown: string;
 
-  const content = xss(marked.parse(markdown), {
-    whiteList: { ...xssDefaults.whiteList, span: ['class'], a: ['target', 'href', 'title', 'rel'] }
-  });
+  const content = markdownToHtml(markdown);
 </script>
 
 {@html content}
---
+++ src/lib/parser.ts
@@ -0,0 +1,30 @@
+import marked from 'marked';
+import type { MarkedOptions } from 'marked';
+import xss, { IFilterXSSOptions } from 'xss';
+import xssDefaults from 'xss/lib/default.js';
+import prism from 'prismjs';
+import 'prismjs/components/prism-javascript.js';
+import 'prismjs/components/prism-typescript.js';
+import 'prismjs/components/prism-python.js';
+import 'prismjs/components/prism-powershell.js';
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-batch.js';
+
+const markedOptions: MarkedOptions = {
+  highlight: (code, lang) => {
+    if (prism.languages[lang]) {
+      return prism.highlight(code, prism.languages[lang], lang);
+    } else {
+      return code;
+    }
+  }
+};
+const xssOptions: IFilterXSSOptions = {
+  whiteList: { ...xssDefaults.whiteList, span: ['class'], a: ['target', 'href', 'title', 'rel'] }
+};
+
+marked.setOptions(markedOptions);
+
+export const markdownToHtml = (markdown: string): string => {
+  return xss(marked.parse(markdown), xssOptions);
+};
