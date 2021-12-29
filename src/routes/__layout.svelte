ee4efb0c24c2dc86b6a8f6d6217d2e35813c37cd
Jen Garcia <jen@haverstack.com>
2021-12-29 12:38:54 -0500

    Add "me" to `rel` value for social media profiles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -31,10 +31,10 @@
       <a class="mr-2" href="/about/">About</a>
     </nav>
     <div class="mb-2">
-      <a class="mr-2" href="https://twitter.com/cuibonobo" target="_blank" rel="noopener noreferrer"
+      <a class="mr-2" href="https://twitter.com/cuibonobo" target="_blank" rel="me noopener noreferrer"
         >Twitter</a
       >
-      <a class="mr-2" href="https://github.com/cuibonobo" target="_blank" rel="noopener noreferrer"
+      <a class="mr-2" href="https://github.com/cuibonobo" target="_blank" rel="me noopener noreferrer"
         >GitHub</a
       >
       <a
