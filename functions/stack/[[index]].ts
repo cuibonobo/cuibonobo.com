c1c091564b2f40d866ee4d412946c3b86ae2ccfe
Jen Garcia <jen@haverstack.com>
2024-10-13 19:10:39 -0400

    feat: Add query language to resources endpoint

--- functions/stack/[[index]].ts
+++ functions/stack/[[index]].ts
@@ -21,7 +21,51 @@ const rejectInvalidConfig = (context: Context<Env>): Response | null => {
   if (!isValidAuth(context)) {
     return new Response(JSON.stringify({ message: 'Forbidden.' }), { status: 400 });
   }
-  return null
+  return null;
+};
+
+const getResource = async (resources: Resources, url: URL): Promise<Response> => {
+  const typeId = url.searchParams.get('type');
+  const contentKey = url.searchParams.get('contentKey');
+  const contentValue = url.searchParams.get('contentValue');
+  const attachmentId = url.searchParams.get('attachmentId');
+  console.log(url.searchParams);
+  if (contentValue && contentKey && typeId) {
+    return Response.json(await resources.getByContentKey(typeId, contentKey, contentValue));
+  }
+  if (contentKey && typeId) {
+    return Response.json(await resources.getContentKey(typeId, contentKey));
+  }
+  if (attachmentId) {
+    return Response.json(await resources.getByAttachmentId(attachmentId));
+  }
+  if (typeId) {
+    return Response.json(await resources.getAllByType(typeId));
+  }
+  return Response.json(await resources.getAll());
+};
+
+const getResourceById = async (resources: Resources, resourceId: string): Promise<Response> => {
+  const result = await resources.getOne(resourceId);
+  if (result == null) {
+    return Response.json({}, { status: 404 });
+  }
+  return Response.json(result);
+};
+
+const postResource = async (resources: Resources, data: ResourceDbInput): Promise<Response> => {
+  return Response.json(await resources.createOne(data));
+};
+
+const postResourceById = async (
+  resources: Resources,
+  resourceId: string,
+  data: Record<string, string>
+): Promise<Response> => {
+  if ('id' in data && data['id'].toLowerCase() != resourceId.toLowerCase()) {
+    return new Response(JSON.stringify({ message: 'Ids do not match!' }), { status: 422 });
+  }
+  return Response.json(await resources.updateOne(resourceId, data));
 };
 
 // Routes
@@ -33,40 +77,31 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   const url = new URL(context.request.url);
   const path = getNormalizedPath(url.pathname, BASE_PATH);
   const pathParts = path.split('/');
-  const resources = Resources(context.env.STACK_DB);
+  const resources = new Resources(context.env.STACK_DB);
   const endpoint = pathParts[0];
   if (endpoint == '') {
     return Response.json(['resources', 'types', 'attachments']);
   }
   if (endpoint == 'resources') {
     if (pathParts.length == 1) {
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'POST':
-          const data: ResourceDbInput = await context.request.json();
-          return Response.json(await resources.createOne(data));
+          return postResource(resources, await context.request.json());
         case 'GET':
-          return Response.json(await resources.getAll());
+          return getResource(resources, url);
         default:
-          return getMethodNotAllowedResponse('GET, POST')
+          return getMethodNotAllowedResponse('GET, POST');
       }
     }
     if (pathParts.length == 2) {
       const resourceId = pathParts[1];
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'POST':
-          const data: Record<string, string> = await context.request.json();
-          if ('id' in data && data['id'].toLowerCase() != resourceId.toLowerCase()) {
-            return new Response(JSON.stringify({ message: 'Ids do not match!' }), { status: 422 });
-          }
-          return Response.json(await resources.updateOne(resourceId, data));
+          return postResourceById(resources, resourceId, await context.request.json());
         case 'DELETE':
           return Response.json(await resources.deleteOne(resourceId));
         case 'GET':
-          const result = await resources.getOne(resourceId);
-          if (result == null) {
-            return Response.json({}, { status: 404 });
-          }
-          return Response.json(result);
+          return getResourceById(resources, resourceId);
         default:
           return getMethodNotAllowedResponse('GET, POST, DELETE');
       }
@@ -74,62 +109,60 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   }
   if (endpoint == 'types') {
     if (pathParts.length == 1) {
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
           return Response.json(await resources.getTypes());
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
     if (pathParts.length == 2) {
       const typeId = pathParts[1];
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
           return Response.json(await resources.getAllByType(typeId));
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
     if (pathParts.length == 3) {
       const typeId = pathParts[1];
       const key = pathParts[2];
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
           return Response.json(await resources.getContentKey(typeId, key));
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
     if (pathParts.length == 4) {
       const typeId = pathParts[1];
       const key = pathParts[2];
       const value = pathParts[3];
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
-          return Response.json(
-            await resources.getByContentKey(typeId, key, value)
-          );
+          return Response.json(await resources.getByContentKey(typeId, key, value));
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
   }
   if (endpoint == 'attachments') {
     if (pathParts.length == 1) {
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
           return Response.json(await resources.getAttachments());
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
     if (pathParts.length == 2) {
       const attachmentId = pathParts[1];
-      switch(context.request.method) {
+      switch (context.request.method) {
         case 'GET':
           return Response.json(await resources.getByAttachmentId(attachmentId));
         default:
-          return getMethodNotAllowedResponse('GET')
+          return getMethodNotAllowedResponse('GET');
       }
     }
   }
